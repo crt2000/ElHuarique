0871317df51b64e20821d901d9b45f45f2d9cc37
Isas2000 <isabelcastro.sara@gmail.com>
2026-09-24 17:26:41 -0500

    Primer cambio

---
+++ src/main/java/MarcosDweb/ElHuarique/ElHuariqueApplication.java
@@ -0,0 +1,13 @@
+package MarcosDweb.ElHuarique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElHuariqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElHuariqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MarcosDweb/ElHuarique/ElHuariqueApplicationTests.java
@@ -0,0 +1,13 @@
+package MarcosDweb.ElHuarique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElHuariqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
