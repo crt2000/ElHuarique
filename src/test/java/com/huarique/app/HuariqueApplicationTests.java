5082ee636ea4d3104e76da84d23846fc239b32aa
Ivan26-c <ivanqg.2607@gmail.com>
2026-09-26 11:39:26 -0500

    Segundo cambio

--- src/test/java/com/huarique/app/HuariqueApplicationTests.java
+++ src/test/java/com/huarique/app/HuariqueApplicationTests.java
@@ -1,13 +1,13 @@
-package MarcosDweb.ElHuarique;
+package com.huarique.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ElHuariqueApplicationTests {
+class HuariqueApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
